e71a0e3ffb7004f0851d21f0ca5bdb231338b8e3
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-07-04 16:46:57 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,11 +4,10 @@ const api = axios.create({
     baseURL: 'https://eventregapp-server.onrender.com/api'
 });
 
-// add token to requests if exists
+// Add token to requests if exists
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
-
     return config;
 });
 
