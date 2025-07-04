e6ed8ad79cba28aa5769ec4e5b6ba9a520938c7a
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-07-04 15:30:29 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://eventregapp-server.onrender.com/api'
 });
 
 // add token to requests if exists
