20be5e78dfc055cbb928f7168d51dcdedd2598f6
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-07-04 19:36:10 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,12 +4,15 @@ const api = axios.create({
     baseURL: 'https://myevent-server.vercel.app/api'
 });
 
-// add token to requests if exists
+// Automatically attach token to every request
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
-    if (token) config.headers.Authorization = token;
+    if (token) {
+        config.headers.Authorization = token;
+    }
     return config;
+}, (error) => {
+    return Promise.reject(error);
 });
-
 
 export default api;
